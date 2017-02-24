5b33e8bed56fcd707638a73e7344b3a0accb397c
trrippy <wes.wong@hotmail.com>
2017-02-24 20:52:35 +0000

    initial setup with routes to nurse page/bed page

---
+++ react-app/src/index.jsx
@@ -0,0 +1,33 @@
+// Application entrypoint.
+
+// Load up the application styles
+// require("../styles/application.scss");
+
+// Render the top-level React component
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+
+
+import Main from './common/main.component.jsx'
+import Home from './common/home.component.jsx'
+
+import Nurse from './nurse/nurse.component.jsx'
+import Bed from './bed/bed.component.jsx'
+
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('react-root')
+// );
+
+ReactDOM.render(
+    <Router history={browserHistory}>
+        <Route component={Main}>
+            <Route path="/" component={Home}/>
+            <Route path="/nurse" component={Nurse} />
+            <Route path="/bed" component={Bed}/>
+        </Route>
+    </Router>,
+    document.getElementById('react-root')
+);
---
+++ react-app/src/nurse/nurse.component.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Nurse extends Component {
+    render(){
+        return (
+          <h1>Nurse Page</h1>
+          );
+    }
+}
+
+export default Nurse
