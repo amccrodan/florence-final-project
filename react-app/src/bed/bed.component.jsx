33fe01cff645afd59e2c82bda0ea6ad4f447a7ee
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-24 22:06:32 +0000

    Fixed indentation.

--- react-app/src/bed/bed.component.jsx
+++ react-app/src/bed/bed.component.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 class Bed extends Component {
-    render(){
-        return (
-          <h1>Bed Page</h1>
-          );
-    }
+  render(){
+    return (
+      <h1>Bed Page</h1>
+    );
+  }
 }
 
 export default Bed
