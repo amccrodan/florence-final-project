1de9a14cfb8d1ed228e0dff7b0d6f57f84fc2baa
trrippy <wes.wong@hotmail.com>
2017-02-24 21:55:07 +0000

    changing to bulma

--- react-app/src/common/main.component.jsx
+++ react-app/src/common/main.component.jsx
@@ -5,24 +5,11 @@ class Main extends Component {
     render(){
         return(
             <div>
-                <nav className="navbar navbar-default">
-                    <div className="container-fluid">
-                        <div className="navbar-header">
-                            <a className="navbar-brand" href="#">Florence</a>
-                        </div>
-                        <div className="navbar-collapse" id="bs-example-navbar-collapse-1">
-                            <ul className="nav navbar-nav">
-                                {/* Change from a to Link */}
-                                <li><Link to="/" activeClassName="active">Home</Link></li>
-                                <li><Link to="/nurse" activeClassName="active">Nurses</Link></li>
-                                <li><Link to="/bed" activeClassName="active">Beds</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                <div className="container">
-                    {this.props.children}
-                </div>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/nurse">Nurses</Link></li>
+                    <li><Link to="/bed">Beds</Link></li>
+                </ul>
             </div>
         );
     }
