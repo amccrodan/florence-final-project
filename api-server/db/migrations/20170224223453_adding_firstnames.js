9b40298a5e38d5f8bba99d1009ff3ca174fff1ae
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-24 22:47:32 +0000

    Added first and last names to patients and nurses

---
+++ api-server/db/migrations/20170224223453_adding_firstnames.js
@@ -0,0 +1,30 @@
+
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.table('nurses', function(t) {
+            t.dropColumn('name');
+            t.string('first_name').notNull();
+            t.string('last_name').notNull();
+        }),
+        knex.schema.table('patients', function(t) {
+            t.dropColumn('name');
+            t.string('first_name').notNull();
+            t.string('last_name').notNull();
+        })
+    ]);
+};
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.table('nurses', function(t) {
+            t.dropColumn('first_name');
+            t.dropColumn('last_name');
+            t.string('name').notNull();
+        }),
+        knex.schema.table('patients', function(t) {
+            t.dropColumn('first_name');
+            t.dropColumn('last_name');
+            t.string('name').notNull();
+        })
+    ]);
+};
