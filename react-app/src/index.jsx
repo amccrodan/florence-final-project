35024407d301fefda0e15745003329bc7c806030
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-25 02:11:41 +0000

    Removed old test code.

--- react-app/src/index.jsx
+++ react-app/src/index.jsx
@@ -8,18 +8,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
-
 import Main from './common/main.component.jsx'
 import Home from './common/home.component.jsx'
 
 import Nurse from './nurse/nurse.component.jsx'
 import Bed from './bed/bed.component.jsx'
-
-
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('react-root')
-// );
 
 ReactDOM.render(
     <Router history={browserHistory}>
