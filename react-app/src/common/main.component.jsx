b1806acdb2c4713cd713fc67ea813d40364eda19
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-26 16:27:46 +0000

    Fixed indentation and whitespace.

--- react-app/src/common/main.component.jsx
+++ react-app/src/common/main.component.jsx
@@ -4,50 +4,37 @@ import { Link } from 'react-router';
 import Login from './login.component.jsx'
 
 class Main extends Component {
-render(){
-  return(
-  <section className="hero is-light is-fullheight">
-
-  <div className="hero-body">
-    <div className="container has-text-centered">
-      <div>
-	    <div className="columns">
-	      <div className="column is-one-third">
-	      </div>
-
-	      <div className="column">
-	      <h1 className="title">
-            Florence
-          </h1>
-          <h3 className="subtitle animated fadeInUp">
-            Your Hospice Assistant
-          </h3>
-	      </div>
-
-	      <div className="column">
-	      </div>
-	    </div>
-
-	  <Login />
-
-		<a className="button is-large">
-    	  <Link to="/nurse" activeClassName="active">Nurses</Link>
-		</a>
-		<a className="button is-large">
-	  	  <Link to="/bed" activeClassName="active">Beds</Link>
-	  	</a>
-
-
-	</div>
-
-    </div>
-  </div>
-
-</section>
-
-
-  );
-}
+  render(){
+    return(
+      <section className="hero is-light is-fullheight">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <div>
+              <div className="columns">
+                <div className="column is-one-third">
+                </div>
+                <div className="column">
+                  <h1 className="title">Florence</h1>
+                  <h3 className="subtitle animated fadeInUp">
+                    Your Hospice Assistant
+                  </h3>
+                </div>
+                <div className="column">
+                </div>
+              </div>
+              <Login />
+              <a className="button is-large">
+                <Link to="/nurse" activeClassName="active">Nurses</Link>
+              </a>
+              <a className="button is-large">
+                <Link to="/bed" activeClassName="active">Beds</Link>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Main
