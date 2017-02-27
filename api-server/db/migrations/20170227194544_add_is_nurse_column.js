94ae1d35361e1e6d16b6d751adc7bb2237afed72
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-27 19:50:55 +0000

    Created new column in nurses table

--- api-server/db/migrations/20170227194544_add_is_nurse_column.js
+++ api-server/db/migrations/20170227194544_add_is_nurse_column.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  knex.schema.tables('nurses', function(t) {
+      t.boolean('is_nurses').default(false).notNull();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  knex.schema.tables('nurses', function(t) {
+      t.dropColumn('is_nurses');
+  })
 };
