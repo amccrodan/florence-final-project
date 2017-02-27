b0e1cfc58c39781e8fa9e380780f08b8082241ac
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-27 19:52:21 +0000

    Modified current nurse seed

--- api-server/db/seeds/02nurses_seed.js
+++ api-server/db/seeds/02nurses_seed.js
@@ -5,11 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('nurses').insert([
-        {id: 1, first_name: 'Joy', last_name: 'McCrodan', active: true, image: 'example.img', password: 1234},
-        {id: 2, first_name: 'Annie', last_name: 'Rademaker', active: false, image: 'example.img', password: 1234},
-        {id: 3, first_name: 'Julia', last_name: 'Wong', active: false, image: 'example.img', password: 1234},
-        {id: 4, first_name: 'Emily', last_name: 'Bamford', active: true, image: 'example.img', password: 1234},
-        {id: 5, first_name: 'Nurse', last_name: 'Master', active: true, image: 'example.img', password: 1234}
+        {id: 1, first_name: 'Joy', last_name: 'McCrodan', active: true, image: 'example.img', password: 1234, is_nurse: true},
+        {id: 2, first_name: 'Annie', last_name: 'Rademaker', active: false, image: 'example.img', password: 1234, is_nurse: true},
+        {id: 3, first_name: 'Julia', last_name: 'Wong', active: false, image: 'example.img', password: 1234, is_nurse: true},
+        {id: 4, first_name: 'Emily', last_name: 'Bamford', active: true, image: 'example.img', password: 1234, is_nurse: true},
+        {id: 5, first_name: 'Nurse', last_name: 'Master', active: true, image: 'example.img', password: 1234, is_nurse: true}
       ]);
     });
 };
