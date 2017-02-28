76f1763373804ff9a56b7b9e99c4a0c68a2cdb9e
Myles Rademaker <mylesprademaker@gmail.com>
2017-02-27 17:49:42 -0800

    Created general outline of pages

---
+++ react-app/src/bed/request-pending.component.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class RequestPending extends Component {
+  render(){
+    return (
+      <section className="hero is-light is-fullheight">
+        <div className="hero nice-background is-fullheight">
+        </div>
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <div>
+              <div className="columns">
+
+                <div className="column is-one-third">
+                </div>
+                <div className="column">
+                  <a className="button is-focused big-circle is-white">Request</a>
+                </div>
+                <div className="column">
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default RequestPending
