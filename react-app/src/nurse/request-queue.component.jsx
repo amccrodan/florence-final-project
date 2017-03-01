42ff1c49a4b40d8ce045c5b5c793d733b09450f4
Steven Bamford <sbamford@alumni.ubc.ca>
2017-03-01 10:14:12 -0800

    Bug fix: updated request key to request_id so each request rendered has unique key

--- react-app/src/nurse/request-queue.component.jsx
+++ react-app/src/nurse/request-queue.component.jsx
@@ -10,7 +10,7 @@ class RequestQueue extends Component {
         </article>
         {this.props.requests.map(request => {
           console.log("Request img should be", request.image);
-          return <Request key={request.id} first_name={request.first_name} last_name={request.last_name} bed_id={request.bed_id} request_type_id={request.request_type_id} img={request.image} />
+          return <Request key={request.request_id} first_name={request.first_name} last_name={request.last_name} bed_id={request.bed_id} request_type_id={request.request_type_id} img={request.image} />
         }
         )}
       </div>
