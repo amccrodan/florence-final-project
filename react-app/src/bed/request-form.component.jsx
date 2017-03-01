46be55061f57cbaefdb9ee3776f8c7783d8db85d
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-03-01 02:31:41 +0000

    Added handler for type buttons, classes to type buttons, and request state change logic.

--- react-app/src/bed/request-form.component.jsx
+++ react-app/src/bed/request-form.component.jsx
@@ -4,13 +4,28 @@ require("../../js/request.js");
 class RequestForm extends Component {
   constructor(props){
     super(props);
-    this.state = {
+      this.state = {
     };
-  this.handleClick = this.handleClick.bind(this);
-
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleTypeButton = this.handleTypeButton.bind(this);
   }
 
-  handleClick (event) {
+  handleTypeButton (event) {
+    let currentType = 0;
+    const requestTypes = ['food', 'bathroom', 'medicine', 'other'];
+    requestTypes.forEach(function (item, index) {
+      if (event.currentTarget.classList.contains(item)) {
+        currentType = index + 1;
+      }
+    });
+    this.props.changeRequestState({request_type_id: currentType}, () => {});
+  }
+
+  handleSubmit (event) {
+    const textarea = document.getElementsByClassName('textarea')[0];
+    this.props.changeRequestState({description: textarea.value}, () => {
+      this.props.postRequest();
+    });
     this.props.changeViewState('requestPending');
   }
 
@@ -31,24 +46,30 @@ class RequestForm extends Component {
           </section>
           <div className='columns'>
             <div className='column'>
-              <span className='button is-large is-primary options'><i className='fa fa-medkit' aria-hidden='true'></i></span>
+              <span className='button is-large is-primary options medicine' onClick={this.handleTypeButton}>
+                <i className='fa fa-medkit' aria-hidden='true'></i>
+              </span>
             </div>
             <div className='column'>
-              <span className='button is-large is-info options'><i className='fa fa-cutlery' aria-hidden='true'></i></span>
+              <span className='button is-large is-info options food' onClick={this.handleTypeButton}>
+                <i className='fa fa-cutlery' aria-hidden='true'></i>
+              </span>
             </div>
             <div className='column'>
-              <span className='button is-large options bathroom'>
+              <span className='button is-large options bathroom' onClick={this.handleTypeButton}>
                 <i className='fa fa-male' aria-hidden='true'></i>
                 <i className='fa fa-female' aria-hidden='true'></i>
               </span>
             </div>
             <div className='column'>
-              <span className='button is-large is-warning options'><i className='fa fa-question' aria-hidden='true'></i></span>
+              <span className='button is-large is-warning options other' onClick={this.handleTypeButton}>
+                <i className='fa fa-question' aria-hidden='true'></i>
+              </span>
             </div>
           </div>
           <p className='input-field'>
             <textarea className='textarea' type='text' placeholder='Add a comment...'></textarea>
-            <button type='submit' className='button is-success submit' onClick={this.handleClick}>
+            <button type='submit' className='button is-success submit' onClick={this.handleSubmit}>
              <h1 className='title is-1'>Submit</h1>
             </button>
           </p>
