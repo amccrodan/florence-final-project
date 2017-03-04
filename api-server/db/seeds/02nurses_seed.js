ed4d261c0c9598c5a46bf3982f8d3658a95cab8c
wesley-wong <wes.wong@hotmail.com>
2017-03-04 15:12:47 -0800

    changed seeds to include localhost:8080 on image

--- api-server/db/seeds/02nurses_seed.js
+++ api-server/db/seeds/02nurses_seed.js
@@ -9,14 +9,14 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('nurses').insert([
-        {first_name: 'Joy', last_name: 'McCrodan', active: true, image: '1.jpg', password: passwordHash, is_nurse: true},
-        {first_name: 'Annie', last_name: 'Rademaker', active: false, image: '5.jpg', password: passwordHash},
-        {first_name: 'Julia', last_name: 'Wong', active: false, image: '6.jpg', password: passwordHash, is_nurse: true},
-        {first_name: 'Emily', last_name: 'Bamford', active: true, image: '7.jpg', password: passwordHash},
-        {first_name: 'Nurse', last_name: 'Master', active: true, image: '8.jpg', password: passwordHash, is_nurse: true},
-        {first_name: 'Joe', last_name: 'Delconte', active: true, image: '2.jpg', password: passwordHash, is_nurse: true},
-        {first_name: 'Trey', last_name: 'Foresberg', active: true, image: '3.jpg', password: passwordHash, is_nurse: true},
-        {first_name: 'Parker', last_name: 'Stark', active: true, image: '4.jpg', password: passwordHash}
+        {first_name: 'Joy', last_name: 'McCrodan', active: true, image: 'http://localhost:8080/images/nurses/1.jpg', password: passwordHash, is_nurse: true},
+        {first_name: 'Annie', last_name: 'Rademaker', active: false, image: 'http://localhost:8080/images/nurses/5.jpg', password: passwordHash},
+        {first_name: 'Julia', last_name: 'Wong', active: false, image: 'http://localhost:8080/images/nurses/6.jpg', password: passwordHash, is_nurse: true},
+        {first_name: 'Emily', last_name: 'Bamford', active: true, image: 'http://localhost:8080/images/nurses/7.jpg', password: passwordHash},
+        {first_name: 'Nurse', last_name: 'Master', active: true, image: 'http://localhost:8080/images/nurses/8.jpg', password: passwordHash, is_nurse: true},
+        {first_name: 'Joe', last_name: 'Delconte', active: true, image: 'http://localhost:8080/images/nurses/2.jpg', password: passwordHash, is_nurse: true},
+        {first_name: 'Trey', last_name: 'Foresberg', active: true, image: 'http://localhost:8080/images/nurses/3.jpg', password: passwordHash, is_nurse: true},
+        {first_name: 'Parker', last_name: 'Stark', active: true, image: 'http://localhost:8080/images/nurses/4.jpg', password: passwordHash}
       ]);
     });
 };
