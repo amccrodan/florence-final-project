729c72c2f906f7163f61ff37c9598b91014331a5
wesley-wong <wes.wong@hotmail.com>
2017-03-06 13:41:56 -0800

    updating /api/patients post route to create new patient

--- api-server/routes/patients.js
+++ api-server/routes/patients.js
@@ -7,6 +7,18 @@ module.exports = (knex) => {
 
   // Create a new patient
   router.post('/'), (req, res) => {
+    // ERD looks like this:
+      // first_name:  'Phil',
+      // last_name: 'Doe',
+      // doctor: 'Dr. Abraham Kennedy',
+      // emergency_contact_name: 'Ken Doe',
+      // emergency_contact_number: '(123)456-7890',
+      // allergies: 'Poor indentation',
+      // previous_injuries: 'none',
+      // recent_illness: 'Strep throat',
+      // notes: 'Thinks Annie is his long lost love', -- can be null
+      // bed_id: 2,
+      // nurse_id: 3
     knex('patients').insert({
       first_name: req.body.first_name,
       last_name: req.body.last_name,
