19bafabc78d3d60a05ba8706116749cbbcfd8d89
wesley-wong <wes.wong@hotmail.com>
2017-03-05 16:18:14 -0800

    added chartjs and react-chart-js, charts now show a doughnut chart which updates every 500ms that show a pie chart of request types 👌

--- react-app/src/admin/charts.component.jsx
+++ react-app/src/admin/charts.component.jsx
@@ -1,14 +1,93 @@
 import React, { PropTypes } from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import {Doughnut} from 'react-chartjs-2';
+import {Bar} from 'react-chartjs-2';
+import axios from 'axios';
+import moment from 'moment';
 
 class Charts extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      data: [],
     };
+    this.getNewData = this.getNewData.bind(this);
+    this.doughtnutData = this.doughtnutData.bind(this);
   }
 
+  componentDidMount () {
+    this.getNewData();
+    setInterval(() => {
+      this.getNewData();
+    }, 5000);
+  }
+
+  getNewData () {
+    axios
+    .get('http://localhost:8080/api/requests')
+    .then((results) => {
+      console.log('request results', results);
+      this.setState({data: results.data});
+      console.log('state', this.state);
+    })
+  }
+
+  doughtnutData(sinceTime) {
+    let food = 0;
+    let bathroom = 0;
+    let medicine = 0;
+    let other = 0;
+    let emergency = 0;
+    this.state.data.forEach(item => {
+      switch(item.request_type_id) {
+        case 1:
+          food += 1;
+          break;
+        case 2:
+          bathroom += 1;
+          break;
+        case 3:
+          medicine += 1;
+          break
+        case 4:
+          other += 1;
+          break;
+        case 5:
+          emergency += 1;
+          break;
+      }
+    })
+    return {
+      labels: [
+        'Food',
+        'Bathroom',
+        'Medicine',
+        'Other',
+        'Emergency',
+      ],
+      datasets: [{
+        data: [food, bathroom, medicine, other, emergency],
+        backgroundColor: [
+        '#FF6384',
+        '#36A2EB',
+        '#FFCE56',
+        '#4BC0C0',
+        '#F0623B',
+        ],
+        hoverBackgroundColor: [
+        '#FF6384',
+        '#36A2EB',
+        '#FFCE56',
+        '#4BC0C0',
+        '#F0623B',
+        ]
+      }]
+    };
+  }
   render () {
+
+    let requestTypeData = this.doughtnutData()
+
     return (
       <ReactCSSTransitionGroup
         transitionName="fadeTransition"
@@ -18,6 +97,7 @@ class Charts extends React.Component {
         transitionLeaveTimeout={300}>
         <div>
           <h1 className='title is-1'>Charts</h1>
+          <Doughnut data={requestTypeData} />
         </div>
       </ReactCSSTransitionGroup>
     );
