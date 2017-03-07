d1a4d7369644d8df8a4e56f706b8547b78fa159e
Myles Rademaker <mylesprademaker@gmail.com>
2017-03-07 15:19:08 -0800

    Fixed whitespace

--- websocket-server/server.js
+++ websocket-server/server.js
@@ -1,20 +1,12 @@
-// server.js
-
 const express = require('express');
 const WebSocket = require('ws');
-
-// Set the port to 4000
 const PORT = 4000;
-
-// Create a new express server
 const server = express()
    // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-// Create the WebSockets server
 const wss = new WebSocket.Server({ server });
-
 // Broadcast to all clients
 wss.broadcast = function broadcast(data) {
   wss.clients.forEach(function each(client) {
