d1f3dcf5dda82d055b88543ae5dd22e5c18c64a4
Myles Rademaker <mylesprademaker@gmail.com>
2017-03-07 10:43:35 -0800

    Table looks good and scroll when overflowed

--- react-app/src/admin/patient.view.component.jsx
+++ react-app/src/admin/patient.view.component.jsx
@@ -36,35 +36,37 @@ class PatientView extends React.Component {
 
   render () {
     return (
-      <ReactCSSTransitionGroup
-        transitionName="fadeTransition"
-        transitionAppear={true}
-        transitionAppearTimeout={500}
-        transitionEnterTimeout={500}
-        transitionLeaveTimeout={300}>
-        <table className='table is-striped'>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Bed</th>
-              <th>Room</th>
-            </tr>
-          </thead>
-          <tbody className='patient-table'>
-          {this.state.patients.map(patient => {
-            return <Patient key={patient.id}
-              id={patient.id}
-              first_name={patient.first_name}
-              last_name={patient.last_name}
-              bed={patient.bed_id}
-              room={patient.room_id}
-              />
-          }
-          )}
-          </tbody>
-        </table>
-      </ReactCSSTransitionGroup>
+      <div className='patient-view'>
+        <ReactCSSTransitionGroup
+          transitionName="fadeTransition"
+          transitionAppear={true}
+          transitionAppearTimeout={500}
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}>
+          <table className='table is-striped'>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Bed</th>
+                <th>Room</th>
+              </tr>
+            </thead>
+            <tbody className='patient-table'>
+            {this.state.patients.map(patient => {
+              return <Patient key={patient.id}
+                id={patient.id}
+                first_name={patient.first_name}
+                last_name={patient.last_name}
+                bed={patient.bed_id}
+                room={patient.room_id}
+                />
+            }
+            )}
+            </tbody>
+          </table>
+        </ReactCSSTransitionGroup>
+      </div>
     );
   }
 }
