9c806c37bc90aeea80ff122c368ec35e69ac8d31
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-03-07 16:44:08 -0800

    Update care-aide.component.jsx
    Fixed indentation of functions.

--- react-app/src/admin/care-aide.component.jsx
+++ react-app/src/admin/care-aide.component.jsx
@@ -3,16 +3,16 @@ class CareAide extends Component{
   constructor(props){
    super(props);
    this.state = {};
-}
+  }
 
-render () {
-  return (
-    <tr className='admin-table-row'>
-      <td className='admin-table-data'>{this.props.id}</td>
-      <td className='admin-table-data'>{this.props.first_name} {this.props.last_name}</td>
-    </tr>
-  );
-}
+  render () {
+    return (
+      <tr className='admin-table-row'>
+        <td className='admin-table-data'>{this.props.id}</td>
+        <td className='admin-table-data'>{this.props.first_name} {this.props.last_name}</td>
+      </tr>
+    );
+  }
 }
 
 export default CareAide;
