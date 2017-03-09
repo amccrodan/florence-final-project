3e11cc0bbc8cbf973fec52557e8aeb96166bfd75
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-03-08 16:23:25 -0800

    Added no requests message when queue is empty.

--- react-app/src/station/request-queue.component.jsx
+++ react-app/src/station/request-queue.component.jsx
@@ -9,39 +9,47 @@ class RequestQueue extends Component {
   }
 
   render(){
+    let output = '';
+    if (this.props.requests.length !== 0) {
+      output = this.props.requests.map(request => {
+        return <Request key={request.request_id}
+          first_name={request.first_name}
+          last_name={request.last_name}
+          bed_id={request.bed_id}
+          request_type_id={request.request_type_id}
+          img={request.image}
+          status_id={request.status_id}
+          created_at={request.created_at}
+          updated_at={request.updated_at}
+          id={request.request_id}
+          nurse_id={request.nurse_id}
+          nurse_first_name={request.nurse_first_name}
+          nurse_last_name={request.nurse_last_name}
+          description={request.description}
+          room_num={request.room_id}
+          notes={request.notes}
+          emerg_name={request.emergency_contact_name}
+          emerg_number={request.emergency_contact_number}
+          doctor={request.doctor}
+          allergies={request.allergies}
+          injury={request.previous_injuries}
+          illness={request.recent_illness}
+          respondToRequest={this.props.respondToRequest}
+          assignStaffToRequest={this.props.assignStaffToRequest}
+          staffSelected={this.props.staffSelected}
+          requestsAssigned={this.props.requestsAssigned}
+          hostName={this.props.hostName}
+          />
+      });
+    } else {
+        output = (<div className='no-requests has-text-centered'>
+          No requests
+        </div>
+      );
+    }
     return (
       <div className='tile is-parent is-vertical is-10 request-queue'>
-        {this.props.requests.map(request => {
-          return <Request key={request.request_id}
-            first_name={request.first_name}
-            last_name={request.last_name}
-            bed_id={request.bed_id}
-            request_type_id={request.request_type_id}
-            img={request.image}
-            status_id={request.status_id}
-            created_at={request.created_at}
-            updated_at={request.updated_at}
-            id={request.request_id}
-            nurse_id={request.nurse_id}
-            nurse_first_name={request.nurse_first_name}
-            nurse_last_name={request.nurse_last_name}
-            description={request.description}
-            room_num={request.room_id}
-            notes={request.notes}
-            emerg_name={request.emergency_contact_name}
-            emerg_number={request.emergency_contact_number}
-            doctor={request.doctor}
-            allergies={request.allergies}
-            injury={request.previous_injuries}
-            illness={request.recent_illness}
-            respondToRequest={this.props.respondToRequest}
-            assignStaffToRequest={this.props.assignStaffToRequest}
-            staffSelected={this.props.staffSelected}
-            requestsAssigned={this.props.requestsAssigned}
-            hostName={this.props.hostName}
-            />
-        }
-        )}
+        {output}
       </div>
     );
   }
